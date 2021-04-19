c43fbd578eff94f9f9b9b7100699750436cdf722
Thiago <thiago.correa@pwbrands.com.br>
2021-04-19 00:46:16 -0300

    Restringindo acessos de listagem e criação

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :faturas
-  resources :matriculas
-  resources :alunos
-  resources :instituicao_de_ensinos
+  resources :faturas, only: [:show, :index]
+  resources :matriculas, only: [:show, :index, :create]
+  resources :alunos, only: [:show, :index, :create]
+  resources :instituicao_de_ensinos, only: [:show, :index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
